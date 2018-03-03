c4a866eef832941b9a105650574925aba9e853e8
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2018-03-03 12:27:01 +0530

    functionality ready; socket working

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/form', function (req, res) {
-   res.sendFile( __dirname + "/form.html" );
+	res.sendFile( __dirname + "/form.html" );
 });
 
 app.get('/display', function (req, res) {
-   res.sendFile( __dirname + "/display.html" );
+	res.sendFile( __dirname + "/display.html" );
 });
 
+app.use(express.static('static'));
+
+
 var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+	console.log("Listening at port 8080");
 });
--- server.js
+++ server.js
@@ -2,14 +2,21 @@ const WebSocket = require('ws');
  
 const wss = new WebSocket.Server({ port: 3000 });
 
+console.log("Waiting for connection at port 3000...")
+
 wss.on('connection', function connection(ws) {
 	ws.on('message', function incoming(data) {
-		// Broadcast to everyone else.
-		wss.clients.forEach(function each(client) {
-			if (client !== ws && client.readyState === WebSocket.OPEN) {
-				client.send(data);
-				console.log(data);
-			}
-		});
+		console.log(data);
+		data = JSON.parse(data);
+
+		if (data.id == "productInfo") {
+			console.log("Broadcasting data");
+			// Broadcast to everyone else.
+			wss.clients.forEach(function each(client) {
+				if (client !== ws && client.readyState === WebSocket.OPEN) {
+					client.send(JSON.stringify(data));
+				}
+			});
+		}
 	});
 });
